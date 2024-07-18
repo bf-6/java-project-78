deb219fcf24017789423750ccb254b395864ab4e
Nikita Ryazanov <ryazanov.na@yandex.ru>
2024-07-18 22:32:58 +0400

    Refactoring 1

--- app/src/test/java/hexlet/code/schemas/SchemasTest.java
+++ app/src/test/java/hexlet/code/schemas/SchemasTest.java
@@ -85,29 +85,29 @@ public class SchemasTest {
     @Test
     public void mapSchemaTestShape() {
 
-        var schemaString = v.map();
+        var schema = v.map();
 
         Map<String, BaseSchema> schemas = new HashMap<>();
 
-        schemas.put("firstName", v.string().required());
-        schemas.put("lastName", v.string().required().minLength(2));
+        schemas.put("firstName", v.string().required().minLength(2));
+        schemas.put("lastName", v.number().required().positive().range(1, 21));
 
-        schemaString.shape(schemas);
+        schema.shape(schemas);
 
-        Map<String, String> human1 = new HashMap<>();
+        Map<String, Object> human1 = new HashMap<>();
         human1.put("firstName", "John");
-        human1.put("lastName", "Smith");
-        assertTrue(schemaString.isValid(human1)); // true
+        human1.put("lastName", 3);
+        assertTrue(schema.isValid(human1)); // true
 
-        Map<String, String> human2 = new HashMap<>();
+        Map<String, Object> human2 = new HashMap<>();
         human2.put("firstName", "John");
         human2.put("lastName", null);
-        assertFalse(schemaString.isValid(human2)); // false
+        assertFalse(schema.isValid(human2)); // false
 
-        Map<String, String> human3 = new HashMap<>();
+        Map<String, Object> human3 = new HashMap<>();
         human3.put("firstName", "Anna");
-        human3.put("lastName", "B");
-        assertFalse(schemaString.isValid(human3)); // false
+        human3.put("lastName", -3);
+        assertFalse(schema.isValid(human3)); // false
 
     }
 
